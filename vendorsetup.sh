89a49b0371a69461461ddb91ac5200d1be199420
nino <pilopilosimon1990@gmail.com>
2026-02-01 20:45:13 +0100

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-git clone https://github.com/ramabondanp/twrp-device_transsion_mt6833-common--H2.git -b fox_12.1 device/transsion/mt6833-common
+git clone https://github.com/nino088/twrp-device_transsion_mt6833-common--H2.git -b fox_12.1 device/transsion/mt6833-common
