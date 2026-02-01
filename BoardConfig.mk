968482b624618afff561f1e558081f3a9e164b2c
nino <pilopilosimon1990@gmail.com>
2026-02-01 19:03:36 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,17 +4,17 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-DEVICE_PATH := device/infinix/Infinix-X666
+DEVICE_PATH := device/itel/P661n
 
 # Inherit from mt6833-common
 include device/transsion/mt6833-common/BoardConfigCommon.mk
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := Infinix-X666
+TARGET_OTA_ASSERT_DEVICE := Itel-P661n
 
 # Init
-TARGET_INIT_VENDOR_LIB := libinit_Infinix-X666
-TARGET_RECOVERY_DEVICE_MODULES := libinit_Infinix-X666
+TARGET_INIT_VENDOR_LIB := libinit_Itel-P661n
+TARGET_RECOVERY_DEVICE_MODULES := libinit_Itel-P661n
 
 # TWRP Configs
-TW_DEVICE_VERSION := X666_by_rama982
+TW_DEVICE_VERSION := P55_5G_By_nino
